e008429b0e1ab4a5231ecd9fd92029196505199a
Alex <mishechkin.aleksei@mail.ru>
2021-01-19 23:36:23 +0300

    Решаю задачки

---
+++ Арифметика.py
@@ -0,0 +1,18 @@
+print('ЗАДАЧА 1')
+print("Выражаем 50 евро в рублях через доллары")
+dollars_count = 50 * 1.25
+print("50 евро = " + str(dollars_count) + " долларов")
+rubles_per_dollars = 60
+print("Курс доллара к рублю = 1 к 60")
+rubles_count = dollars_count * rubles_per_dollars
+print("50 долларов = " + str(rubles_count) + "рублей")
+print()
+print('ЗАДАЧА 2')
+print('Дано 100 евро')
+print('1 евро = 1.25 долларов\n1 доллар = 60 рублей')
+euro_count = 100
+dollars_count = euro_count * 1.25
+print('100 евро = ' + str(dollars_count) + ' долларов')
+rubles_per_dollars = 60
+rubles_count = dollars_count * rubles_per_dollars
+print('100 долларов = ' + str(rubles_count) + 'рублей')
