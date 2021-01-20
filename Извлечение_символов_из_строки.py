4c895a1e9c187e0f1e0d5b6a1b4d9d784f61e24b
Alex <mishechkin.aleksei@mail.ru>
2021-01-20 23:51:16 +0300

    Извлекал элемент по индексу

---
+++ Извлечение_символов_из_строки.py
@@ -0,0 +1,10 @@
+one = 'Naharis'
+two = 'Mormont'
+three = 'Sand'
+print(one[2])
+print(two[1])
+print(three[3])
+print(two[4])
+print(two[2])
+name = '{}{}{}{}{}'
+print(name.format(one[2], two[1], three[3], two[4], two[2]))
