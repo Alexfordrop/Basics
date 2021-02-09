8e018683751129e5b31e3475f1155fefac753ef8
Alex <mishechkin.aleksei@mail.ru>
2021-02-09 23:40:46 +0300

    блок finally

--- raising.py
+++ raising.py
@@ -12,7 +12,7 @@ try:
 except EOFError:
     print('Ну зачем вы сделали мне EOF?')
 except ShortInputException as ex:
-    print('ShortInputException: ждинна введённой строки -- {0}; \
+    print('ShortInputException: длинна введённой строки -- {0}; \
         ожидалось, как минимум, {1}'.format(ex.length, ex.atleast))
 else:
     print('Не было исключений.')
