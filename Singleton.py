489f21a89bef09c5bb94e6a40a39851fda9e8b1d
Alex <mishechkin.aleksei@mail.ru>
2021-02-16 23:51:14 +0300

    Закрепляю ООП

---
+++ Singleton.py
@@ -0,0 +1,15 @@
+class Singleton(object):
+    obj = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.obj is None:
+            cls.obj = object.__new__(cls, *args, **kwargs)
+        return cls.obj
+
+single = Singleton()
+single.attr = 42
+newSingle = Singleton()
+newSingle.attr
+newSingle is single
+
+print(newSingle.attr)
