db863d9875d74042245345bf4d5099764b12d1fd
Alex <mishechkin.aleksei@mail.ru>
2021-02-23 23:36:56 +0300

    Закрепляю ООП

---
+++ duck_tip.py
@@ -0,0 +1,15 @@
+class English:
+    def greeting(self):
+        print('Hello')
+
+class French:
+    def greeting(self):
+        print('Bonjour')
+
+def intro(language):
+    language.greeting()
+
+john = English()
+gerard = French()
+intro(john)
+intro(gerard)
