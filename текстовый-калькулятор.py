5e0c41ba52071c81136a5f5e225e74fb6cdf3336
Alex <mishechkin.aleksei@mail.ru>
2021-03-08 23:31:09 +0300

    Простой текстовый калькулятор

---
+++ текстовый-калькулятор.py
@@ -0,0 +1,24 @@
+print('Введите число a = ')
+a = int(input())
+
+print('Введите число b = ')
+b = int(input())
+
+print('Выбирите операцию')
+print('1 - сложить a + b')
+print('2 - вычесть a - b')
+print('3 - умножить a * b')
+print('4 - разделить a / b')
+d = int(input())
+
+if d == 1:
+    print('Сумма a + b =', a + b)
+
+if d == 2:
+    print('Разность a - b =', a - b)
+
+if d == 3:
+    print('Произведение a * b =', a * b)
+
+if d == 4:
+    print('Частное a / b =', a / b)
