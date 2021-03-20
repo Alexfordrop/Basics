aef842006eea6d72314c38c0dd904463cd187e7c
Alex <mishechkin.aleksei@mail.ru>
2021-03-20 23:59:18 +0300

    Цикл for

---
+++ немного_циклов.py
@@ -0,0 +1,19 @@
+
+for a in range (0, 5):
+    print('привет')
+    print("Ура!!!")
+
+print(list(range(10, 17)))
+
+sp1 = [10, 40, 50, 77]
+sp2 = ["must", "yes", "no"]
+for i in range(0, 5):
+    for j in range(0, 5):
+        print(i, j)
+
+found_coins = 20
+magic_coins = 10
+stolen_coins = 2
+coins = found_coins
+
+print(found_coins + 365 * (magic_coins - stolen_coins))
