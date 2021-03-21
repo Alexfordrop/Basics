a7e578a0d43a0bf0ba18dd67678a80c23a0db29f
Alex <mishechkin.aleksei@mail.ru>
2021-03-21 23:44:54 +0300

    Функция range

---
+++ range_me.py
@@ -0,0 +1,15 @@
+for i in range(5):
+    print(i)
+
+arr = ["Hi", "World", "We", "Are", "No"]
+for j in range(len(arr)):
+    print(arr[j])
+
+for k in range(10, 20):
+    print(k)
+
+for t in range(-5, -10):
+    print(t)
+
+for y in range(10, 50, 5):
+    print(y)
