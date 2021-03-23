24a9052e5b50291bc98c4411ecac3135dd5df6b4
Alex <mishechkin.aleksei@mail.ru>
2021-03-24 00:00:31 +0300

    Цикл While

--- test_ok.py
+++ test_ok.py
@@ -1,5 +1,6 @@
-d = ['sf', 'teg', 'qwert', 'er', 'xvb']
-while d:
-    print(d.pop())
-else:
-    print('1234'
+x = 120
+y = 133
+while x * 20 < 1000 and y * 20<1200:
+    print(x, y)
+    x = x + 100
+    y = y + 200
---
+++ цикл_while.py
@@ -0,0 +1,15 @@
+i = 0
+while True:
+    print(i)
+    i = i + 1 
+    if i == 100:
+        break
+
+x = 120
+y = 133
+
+while x * 20 < 1000 and y * 20<1200:
+    print(x, y)
+    x = x + 100
+    y = y + 200
+    
