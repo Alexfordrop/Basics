6754143a41648e9574b0f4390fc2d57ea3192245
Alex <mishechkin.aleksei@mail.ru>
2021-04-03 23:33:23 +0300

    Функции

---
+++ new_func.py
@@ -0,0 +1,13 @@
+def summa2(a, b, c = 100):
+    return a + b + c
+
+ab = summa2(2, 30)
+print("Сумма a + b =", ab)
+
+ab = summa2(2, 30, 3000)
+print("Сумма a + b =", ab)
+
+def p1(a, b):
+    print("a =", a, "b =", b)
+
+p1(33, 77)
