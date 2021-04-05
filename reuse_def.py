d696ebceedf795b7da1499e6788aaf73f618fdf4
Alex <mishechkin.aleksei@mail.ru>
2021-04-05 23:49:23 +0300

    Функции

---
+++ reuse_def.py
@@ -0,0 +1,8 @@
+def podschet_i_ves(monety, ves, nomer):
+    print("Подсчёт №%s" % nomer)
+    print("Всего вес монет: %s" % (monety * ves))
+
+# print("Подсчёт №1")
+podschet_i_ves(100, 2.5, 1)
+# print("Подсчёт №2")
+podschet_i_ves(200, 5, 2)
