65f5bc55542d8cd2913be7b606e30ffc7bdb4457
Alex <mishechkin.aleksei@mail.ru>
2021-04-08 23:17:21 +0300

    Мои модули

---
+++ module_1.py
@@ -0,0 +1,8 @@
+my_var = 3.2356757
+
+def my_function():
+    print(my_var)
+    print("Моя функция в моем модуле 1")
+
+for i in range(0, 5):
+    print(i)
---
+++ somemodul.py
@@ -0,0 +1,13 @@
+from math import *
+
+print(sqrt(100))
+
+print(sqrt(9))
+
+print(pi)
+
+import module_1
+
+print(module_1.my_var)
+
+module_1.my_function()
