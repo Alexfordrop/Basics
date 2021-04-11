a607f8ff125c4fae7e63ba7a51574b8cc811ef4a
Alex <mishechkin.aleksei@mail.ru>
2021-04-11 22:15:08 +0300

    Some module

--- something.py
+++ something.py
@@ -1,2 +1,29 @@
-def a():
+import copy
+
+class Auto():
     pass
+
+my_auto1 = Auto()
+my_auto1.wheels = 4
+print(my_auto1.wheels)
+
+my_auto2 = copy.copy(my_auto1)
+my_auto2.wheels = 8
+
+print(my_auto1.wheels)
+print(my_auto2.wheels)
+
+##
+
+import keyword
+
+print(keyword.iskeyword('if'))
+print(keyword.iskeyword('while'))
+print(keyword.iskeyword('match'))
+
+##
+
+import random
+
+num =
+print(random.randint(1, 100))
