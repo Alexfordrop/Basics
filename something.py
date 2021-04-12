05b1d327d1e95ad1aa0204ff765c1c73e9f80d38
Alex <mishechkin.aleksei@mail.ru>
2021-04-12 21:57:30 +0300

    Some modules

--- something.py
+++ something.py
@@ -25,5 +25,36 @@ print(keyword.iskeyword('match'))
 
 import random
 
-num =
-print(random.randint(1, 100))
+num = random.randint(1, 100)
+'''while True:
+    print("Введите число от 1 до 100:")
+    num1 = int(input())
+    if num1 == num:
+        print("Вы угадали!")
+        break
+    else:
+        print("Вы не угадали!")
+    if num1 == 1000:
+        print("Загаданное число %s" % num)'''
+
+sp1 = ['ауди', 'мазда', 'бмв', 'тойота']
+print(random.choice(sp1))
+
+import sys
+
+# in1 = sys.stdin.readline()
+#sys.stdout.write(in1)
+print(sys.version)
+
+import pickle
+
+game = {'life' : 5, 'armor' : 7, 'level' : 100}
+file1 = open('testg.txt', 'wb')
+pickle.dump(game, file1)
+file1.close()
+
+load_file = open('testg.txt', 'rb')
+loaded = pickle.load(load_file)
+load_file.close()
+
+print(loaded)
