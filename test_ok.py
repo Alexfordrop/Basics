23cb0c4dfa1107dffe11ca5a15cb8fcce6470dfa
Alex <mishechkin.aleksei@mail.ru>
2021-04-28 23:50:09 +0300

    Сортировка

--- test_ok.py
+++ test_ok.py
@@ -1,4 +1,11 @@
-students = ['Ivan', 'Masha', 'Sasha']
-students += ['Olga']
-students += 'Olga'
-print(students)
+a = [int(i) for i in input().split()]
+if len(a) == 1:
+    print(a[0])
+for i in a:
+    if i == 0:
+        [i + 1] + [-1]
+    elif i == -1:
+        [-2] + [0]
+    else:
+        [i-1] + [i+1] % len(a)
+print(*a)
