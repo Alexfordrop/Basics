78ddeb32627254acc342209491b11fc1acfdc5e4
Alex <mishechkin.aleksei@mail.ru>
2021-04-29 23:51:44 +0300

    Паттерн наблюдатель

--- test_ok.py
+++ test_ok.py
@@ -1,11 +1,3 @@
-a = [int(i) for i in input().split()]
-if len(a) == 1:
-    print(a[0])
-for i in a:
-    if i == 0:
-        [i + 1] + [-1]
-    elif i == -1:
-        [-2] + [0]
-    else:
-        [i-1] + [i+1] % len(a)
-print(*a)
+a = list(map(int, input().split()))
+a.sort()
+print(*set([a[i] for i in range(1, len(a)) if a[i] == a[i-1]]) ,end= "")
