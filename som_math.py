3acf51616091bd763f7788bc206f6a692b7b5ff1
Alex <mishechkin.aleksei@mail.ru>
2021-05-29 23:31:59 +0300

    -

---
+++ som_math.py
@@ -0,0 +1,9 @@
+import math
+radius = 30
+# площадь круга с радиусом 30
+area = math.pi * math.pow(radius, 2)
+print(area)
+ 
+# натуральный логарифм числа 10
+number = math.log(10, math.e)
+print(number)
