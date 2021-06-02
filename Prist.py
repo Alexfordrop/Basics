d1392547c13f7dc92c1dab18955c12de7c9c5780
Alexfordrop <77411873+Alexfordrop@users.noreply.github.com>
2021-06-02 23:44:28 +0300

    Update Prist.py

--- Prist.py
+++ Prist.py
@@ -8,3 +8,17 @@ for i in daily:
 
 def things(number, time)
   pass
+
+s = input()
+ans = ''
+anscnt = 0
+for i in range(len(s)):
+  nowcnt = 0
+  for j in range(len(s)):
+    if s[i] == s[j]:
+      nowcnt += 1
+  if nowcnt > anscnt:
+    ans = s[i]
+    anscnt = nowcnt
+    
+print(ans)
