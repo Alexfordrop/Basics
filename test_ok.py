dbda7a7f8a3b4d4172cc125d21d6d4cc5212dc3a
Alex <mishechkin.aleksei@mail.ru>
2021-06-06 23:53:39 +0300

    Алгоритмическая задачка

--- test_ok.py
+++ test_ok.py
@@ -1,7 +1,4 @@
-num = 234564
-num1 = str(num)
-list1 = list(num1)
-list2 = sorted(list1, reverse=True)
-string_1 = ''.join(list2)
-final_integer = int(string_1)
-print(final_integer)
+def multi_table(number):
+    for i in range(10):
+        a = i * number
+        return '{} * {} = '.format(i, number), a
