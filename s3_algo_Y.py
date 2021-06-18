a907742d7804e8e96ab65dec941ade6318a25164
Alex <mishechkin.aleksei@mail.ru>
2021-06-18 13:18:43 +0300

    -

--- s3_algo_Y.py
+++ s3_algo_Y.py
@@ -1,1 +1,13 @@
 a = {}
+b = dict(short='dict', long='dictionary')
+print(b)
+d = {a: a ** 2 for a in range(7)}
+print(d)
+
+print(a.get(2))
+
+d2 = {"A1":"123", "A2":"456"}
+print("A1" in d2)
+print("A3" in d2)
+print(d2.keys())
+print(d2.popitem())
