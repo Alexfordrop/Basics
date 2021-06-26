74f07497438cf665feb2dfb83191084116c856c3
Alex <mishechkin.aleksei@mail.ru>
2021-06-26 23:44:16 +0300

    Деревья

--- new_tree.py
+++ new_tree.py
@@ -1,5 +1,31 @@
-def maketree(serialised):
+def maketree(serialized):
     tree ={'left': None, 'right': None, 
 'up': None, 'type': 'root'}
     nownode = tree
-    for sym in
+    for sym in serialized:
+        if sym == 'D':
+            newnode = {'left': None, 'right': None, 
+'up': nownode, 'type': 'left'}
+            nownode['left'] = newnode
+            nownode = newnode
+        elif sym == 'U':
+            while nownode['type'] == 'right':
+                nownode = nownode['up']
+            nownode = nownode['up']
+            nownode = {'left': None, 'right': None, 
+'up': nownode, 'type': 'right'}
+            nownode['right'] = newnode
+            nownode = newnode
+    return tree
+
+def traverse(root, prefix):
+    if root['left'] is None and root['right'] is None:
+        return [''.join(prefix)]
+    prefix.append('0')
+    ans = traverse(root['left'], prefix)
+    prefix.pop()
+    ptefix.append('1')
+
+    ans.extend(traverse(root['right'], prefix))
+    prefix.pop()
+    return ans
--- test_ok.py
+++ test_ok.py
@@ -1,4 +1,4 @@
-def multi_table(number):
-    for i in range(10):
-        a = i * number
-        return '{} * {} = '.format(i, number), a
+a = [for i in input()]
+for n in range(a):
+    if a[0] + a[1] == 0:
+        m = (a[0] ** 2) + (a[1] ** 2)
